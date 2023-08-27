7bf607f20dea71a863677edaf7703af02a298657
vohaiphuc <vohaiphuc@gmail.com>
2023-08-27 22:19:29 +0700

    pagination vip pro

--- app/controllers/v2/controller.js
+++ app/controllers/v2/controller.js
@@ -1,10 +1,17 @@
 export const FOOD_URL = "https://64d6fb672a017531bc12e8b5.mockapi.io/food"
+let FOOD_LIST_RAW = []
+const MAX_ITEM = 10
+let CURRENT_PAGE = 0
 
-let renderList = (list) => {
+/**
+ * Rendering functions
+ */
+
+let renderTable = (list) => {
     let DEFAULT_MON_CHAY = true
     let DEFAULT_TINH_TRANG = true
     let contentHtml = ""
-    list.reverse().forEach(food => {
+    list.forEach(food => {
         contentHtml += /*html*/`
             <tr>
                 <td>${food.ma}</td>
@@ -24,41 +31,82 @@ let renderList = (list) => {
     $("#tbodyFood")[0].innerHTML = contentHtml
 }
 
+window.renderPagination = (page = 0) => {
+    let list = FOOD_LIST_RAW
+    // Split array
+    let FOOD_LIST_SPLIT = []
+    let i = 0
+    while (i < list.length) {
+        let pageArr = []
+        let j = i
+        while (j < MAX_ITEM + i && list[j] !== undefined) {
+            pageArr.push(list[j])
+            j++
+        }
+        i = i + pageArr.length
+        FOOD_LIST_SPLIT.push(pageArr)
+    }
+
+    // Update current page
+    // FOOD_LIST_SPLIT[page] === undefined khi xóa hết item của trang hiện tại => Render list của trang trước
+    CURRENT_PAGE = (FOOD_LIST_SPLIT[page] !== undefined) ? page : page - 1
+
+    // Render pagination
+    let pages = Math.ceil(list.length / MAX_ITEM)
+    let paginationHtml = ``
+    for (let i = 1; i <= pages; i++) {
+        let active = (i - 1) == CURRENT_PAGE ? " active" : ""
+        paginationHtml += `
+            <li class="page-item ${active}"><a class="page-link" href="#" onclick="renderPagination(${i - 1})">${i}</a></li>
+        `
+    }
+    $("#renderPages")[0].innerHTML = paginationHtml
+
+    // Render table
+    renderTable(FOOD_LIST_SPLIT[CURRENT_PAGE])
+}
+
+export const prev = () => {
+    if (CURRENT_PAGE > 0) {
+        CURRENT_PAGE--
+        renderPagination(CURRENT_PAGE)
+    }
+}
+export const next = () => {
+    if (CURRENT_PAGE < MAX_ITEM) {
+        CURRENT_PAGE++
+        renderPagination(CURRENT_PAGE)
+    }
+}
+
 export const fetchFoodList = () => {
+    toggleSpinner()
     axios.get(FOOD_URL)
         .then((res) => {
             let list = res.data
-            renderList(list);
+            FOOD_LIST_RAW = list
+            renderPagination(CURRENT_PAGE)
+            toggleSpinner()
         })
         .catch((err) => {
             console.log(err);
+            toggleSpinner()
         });
 }
 
-// MODAL
-export const openModal = (mode = true) => {
-    $("#exampleModal").modal(mode ? "show" : "hide")
-}
-
-// get / post data form
+/**
+ * Input form functions
+ */
 export const getDataForm = () => {
-    let ma = document.getElementById("foodID").value
-    let ten = document.getElementById("tenMon").value
-    let loai = document.getElementById("loai").value == "loai1"
-    let gia = document.getElementById("giaMon").value
-    let khuyenMai = document.getElementById("khuyenMai").value
-    let tinhTrang = document.getElementById("tinhTrang").value == "1"
-    let hinhAnh = document.getElementById("hinhMon").value
-    let moTa = document.getElementById("moTa").value
     return {
-        ma,
-        ten,
-        loai,
-        gia,
-        khuyenMai,
-        tinhTrang,
-        hinhAnh,
-        moTa,
+        ma: document.getElementById("foodID").value,
+        ten: document.getElementById("tenMon").value,
+        loai: document.getElementById("loai").value == "loai1",
+        gia: document.getElementById("giaMon").value,
+        khuyenMai: document.getElementById("khuyenMai").value,
+        tinhTrang: document.getElementById("tinhTrang").value == "1",
+        hinhAnh: document.getElementById("hinhMon").value,
+        moTa: document.getElementById("moTa").value,
     }
 }
 
@@ -72,4 +120,40 @@ export const pushDataForm = (food) => {
     document.getElementById("tinhTrang").value = tinhTrang ? "1" : "0"
     document.getElementById("hinhMon").value = hinhAnh
     document.getElementById("moTa").value = moTa
+}
+
+export const resetForm = () => {
+    document.getElementById("foodID").value = ""
+    document.getElementById("tenMon").value = ""
+    document.getElementById("loai").value = ""
+    document.getElementById("giaMon").value = ""
+    document.getElementById("khuyenMai").value = ""
+    document.getElementById("tinhTrang").value = ""
+    document.getElementById("hinhMon").value = ""
+    document.getElementById("moTa").value = ""
+    $("#foodID")[0].disabled = false
+}
+
+/**
+ * Extension functions
+ */
+// MODAL
+export const openModal = (mode = true) => {
+    $("#exampleModal").modal(mode ? "show" : "hide")
+}
+
+// SPINNER
+export const toggleSpinner = () => {
+    $(".loading")[0].classList.toggle("d-flex")
+}
+
+// Toast
+export const showMessage = (mess) => {
+    Toastify({
+        text: mess,
+        className: "info",
+        style: {
+            background: "linear-gradient(to right, #00b09b, #96c93d)",
+        }
+    }).showToast();
 }
--- app/controllers/v2/main.js
+++ app/controllers/v2/main.js
@@ -2,63 +2,96 @@
 // 2. Thêm - done
 // 3. Sửa - done
 // 4. Xóa - done
-// 5. preloader
-// 6. toast
+// 5. Spinner - done
+// 6. Toast - done
+// 7. Pagination PRO VIP
 
-import { FOOD_URL, fetchFoodList, openModal, getDataForm, pushDataForm } from "./controller.js"
+import { FOOD_URL, fetchFoodList, openModal, getDataForm, pushDataForm, resetForm, toggleSpinner, showMessage, prev, next } from "./controller.js"
 
 fetchFoodList()
 
-// testing
-// openModal()
-
 window.addFood = () => {
+    toggleSpinner()
     let newFood = getDataForm()
     axios.post(FOOD_URL, newFood)
         .then((res) => {
             fetchFoodList()
             openModal(false)
+            toggleSpinner()
+            showMessage("Thành công")
         })
         .catch((err) => {
             console.log(err);
+            openModal(false)
+            toggleSpinner()
+            showMessage("Có lỗi xảy ra")
         });
 }
 
 window.editFood = (id) => {
+    toggleSpinner()
     openModal()
+    $("#foodID")[0].disabled = true
+    $("#btnCapNhat")[0].disabled = false
     let url = `${FOOD_URL}/${id}`
     axios.get(url)
         .then((res) => {
             console.log(res);
             let food = res.data
             pushDataForm(food)
+            toggleSpinner()
         })
         .catch((err) => {
             console.log(err);
+            toggleSpinner()
+            showMessage("Có lỗi xảy ra")
         });
 }
 
 window.updateFood = () => {
+    toggleSpinner()
     let food = getDataForm()
     let url = `${FOOD_URL}/${food.ma}`
     axios.put(url, food)
         .then((res) => {
             fetchFoodList()
             openModal(false)
+            toggleSpinner()
+            showMessage("Thành công")
         })
         .catch((err) => {
             console.log(err);
+            openModal(false)
+            toggleSpinner()
+            showMessage("Có lỗi xảy ra")
         });
 }
 
 window.deleteFood = (id) => {
+    toggleSpinner()
     let url = `${FOOD_URL}/${id}`
     axios.delete(url)
         .then((res) => {
             fetchFoodList()
-            console.log(res);
+            toggleSpinner()
+            showMessage("Thành công")
         })
         .catch((err) => {
             console.log(err);
+            openModal(false)
+            toggleSpinner()
+            showMessage("Có lỗi xảy ra")
         });
+}
+
+window.resetForm = () => {
+    resetForm()
+    $("#btnCapNhat")[0].disabled = true
+}
+
+window.prev = () => {
+    prev()
+}
+window.next = () => {
+    next()
 }
